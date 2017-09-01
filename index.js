fb54c50cccb6ce2ec750ba6ea8bdd556a06feeb6
jingram-classy <jingram@classy.org>
2017-09-01 10:48:08 -0700

    Add additional methods.

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 'use strict';
-const request = require('request');
+const httpRequest = require('request');
 const _ = require('lodash');
-let apiUrl;
-let token;
-let secret;
-let timeout;
 let HmacAuthorize;
+let config;
 
 function parse(body) {
   try {
@@ -16,11 +13,12 @@ function parse(body) {
   }
 }
 
-function payRequest(appId, method, resource, payload, callback) {
+function request(appId, method, resource, payload, callback) {
   let options = {
-    url: `${apiUrl}${resource}?appId=${appId}&meta=true`,
-    timeout: timeout,
+    url: `${config.apiUrl}${resource}?appId=${appId}&meta=true`,
+    timeout: config.timeout,
     method,
+    agent: config.agent,
     headers: {
       'Authorization': HmacAuthorize.sign(method, resource, 'application/json',
         payload ? JSON.stringify(payload) : null),
@@ -31,7 +29,7 @@ function payRequest(appId, method, resource, payload, callback) {
     options.headers['Content-Type'] = 'application/json';
     options.body = JSON.stringify(payload);
   }
-  request(options, function(error, response, body) {
+  httpRequest(options, function(error, response, body) {
     callback(error, {
       status: _.get(response, 'statusCode'),
       error,
@@ -42,21 +40,47 @@ function payRequest(appId, method, resource, payload, callback) {
   });
 }
 
-module.exports = (config) => {
-  if (!config || !config.apiUrl || !config.timeout ||
-    !config.token || !config.secret) {
-    throw new Error('You must provide apiUrl, timeout, token, and secret.');
+function forObject(appId, method, resource, body, callback) {
+  request(appId, method, resource, body, function(error, result) {
+    if (_.get(result, 'status') !== 200) {
+      callback(error || `${result.status}: ${resource} ${result.body}`);
+    } else {
+      callback(null, result.object);
+    }
+  });
+}
+
+function get(appId, resource, callback) {
+  return forObject(appId, 'GET', resource, null, callback);
+}
+
+function post(appId, resource, object, callback) {
+  return forObject(appId, 'POST', resource, object, callback);
+}
+
+function put(appId, resource, object, callback) {
+  return forObject(appId, 'PUT', resource, object, callback);
+}
+
+function del(appId, resource, callback) {
+  return forObject(appId, 'DELETE', resource, null, callback);
+}
+
+module.exports = (callerConfig) => {
+  config = callerConfig;
+  if (!config || !config.apiUrl || !config.token || !config.secret) {
+    throw new Error('You must provide apiUrl, token, and secret.');
   }
-  apiUrl = config.apiUrl;
-  token = config.token;
-  secret = config.secret;
-  timeout = config.timeout;
   HmacAuthorize = require('authorization-hmac256')({
     service: 'CWS',
-    token,
-    secret
+    token: config.token,
+    secret: config.secret
   });
   return {
-    request: payRequest
+    request,
+    get,
+    post,
+    put,
+    del
   };
 };
