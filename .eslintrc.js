1b4b47b4ad0b48bee9cedb099fc18b16b476081e
Jeremy Brown <jbrown@classy.org>
2018-01-19 10:42:03 -0800

    stop using global variables

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,6 +18,7 @@ module.exports = {
     "node": true
   },
   "rules": {
+    "max-len": 0, // max line length 80 chars
     "require-jsdoc": 0, // require JSDoc comments,
     "comma-dangle": 0, // enforce trailing commas,
     "arrow-parens": 0 // enforce parens arround single arg arrow fcts
